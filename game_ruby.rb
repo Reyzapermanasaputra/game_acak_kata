4d7ba38ac2e1a2014cec475e0e597b14cf141a82
Reyza Permana <reyza.permana@wgs.co.id>
2018-09-12 16:49:38 +0700

    fix error

--- game_ruby.rb
+++ game_ruby.rb
@@ -6,7 +6,7 @@ class Game
   end 
 
   def welcome
-  	welcome = <<-STR
+    welcome = <<-STR
     # ------------------------------------------------------ #
     # Welcome to the Game Acak Kata #
     # ------------------------------------------------- ---- #
@@ -15,30 +15,30 @@ class Game
   end
 
   def show_point
-  	@point += 1
+    @point += 1
   end
   
   def self.play
-  	game = Game.new()
+    game = Game.new()
     words = ["buku", "pencil", "penggaris", "meja", "botol", "komputer", "laptop", "mouse", "kursi"].shuffle
     game.welcome
     i = 0
     while i < 1
-	    words.each do |word|
-		    puts "  Tebak Kata : '#{word.chars.shuffle.join()}'"
-		    print "Jawaban Anda : "
-		    jawaban = gets.chomp.to_str
-		    if jawaban.eql?(word)
-		      puts "Benar!! point anda "+  game.show_point.to_s
-		    else
-		      puts "Upps Anda salah, harap dicoba lagi!!"
-		    end
-		  end
-		  puts "total skor anda : " + game.show_point.to_s
-		  print "  Mau main lagi ? ketik ya/tidak \n "
-		  main_lagi = gets.chomp.to_str
-		  main_lagi.eql?("ya") ? i = 0 : i = 2
-		end
+      words.each do |word|
+        puts "  Tebak Kata : '#{word.split('').shuffle.join()}'"
+        print "Jawaban Anda : "
+        jawaban = gets.chomp.to_str
+        if jawaban.eql?(word)
+          puts "Benar!! point anda "+  game.show_point.to_s
+        else
+          puts "Upps Anda salah, harap dicoba lagi!!"
+        end
+      end
+      puts "total skor anda : " + game.show_point.to_s
+      print "  Mau main lagi ? ketik ya/tidak \n "
+      main_lagi = gets.chomp.to_str
+      main_lagi.eql?("ya") ? i = 0 : i = 2
+    end
   end
 end
 Game.play
